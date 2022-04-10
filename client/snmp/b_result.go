b1cb110c54e2fe6b103542951becb48f395f95be
gondril <alfred.gondril@gmail.com>
2022-04-10 22:10:04 +0700

    detail result

--- client/snmp/b_result.go
+++ client/snmp/b_result.go
@@ -14,8 +14,15 @@ func walkValue(results []gosnmp.SnmpPDU) {
 		case gosnmp.OctetString:
 			fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
 
-		default:
-			fmt.Printf("number: %d\n", gosnmp.ToBigInt(variable.Value))
+		case gosnmp.Integer:
+			fmt.Printf("integer: %v\n", gosnmp.ToBigInt(variable.Value))
+
+		case gosnmp.Counter32:
+			fmt.Printf("count32: %v\n", variable.Value)
+
+		case gosnmp.OpaqueFloat:
+			fmt.Printf("float: %f\n", variable.Value)
+
 		}
 	}
 }
@@ -28,8 +35,15 @@ func getValue(result *gosnmp.SnmpPacket) {
 		case gosnmp.OctetString:
 			fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
 
-		default:
-			fmt.Printf("number: %d\n", gosnmp.ToBigInt(variable.Value))
+		case gosnmp.Integer:
+			fmt.Printf("integer: %v\n", gosnmp.ToBigInt(variable.Value))
+
+		case gosnmp.Counter32:
+			fmt.Printf("count32: %v\n", variable.Value)
+
+		case gosnmp.OpaqueFloat:
+			fmt.Printf("float: %f\n", variable.Value)
+
 		}
 	}
 }
